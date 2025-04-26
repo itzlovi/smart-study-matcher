87d99fbc3cec835de2aadc583c45ec15b33f61d0
itzlovi <145151218+itzlovi@users.noreply.github.com>
2025-04-26 14:47:20 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // PostgreSQL database connection
 const pool = new Pool({
